41230aab3e44f31ba82b7eaeed55c6089d53b2bd
Max <maxim.tat.1988@gmail.com>
2022-10-21 09:42:13 +0300

    new changes

--- tasks/fourth_task.py
+++ tasks/fourth_task.py
@@ -1,15 +1,13 @@
 from collections import namedtuple
 
 from data.data import CsvLoader
+from .first_task import FirstTask
 
 
-class FourthTask():
-
-    def __init__(self, client_id) -> None:
-        self.client_id = client_id
+class FourthTask(FirstTask):
 
     def get_hotspots_quality(self):
-        hotspots = CsvLoader.load("hotspots")
+        hotspots = CsvLoader.load(self.hotspots_file)
         good_hotspots = hotspots.id[hotspots.owner_id == self.client_id][hotspots.score_v4 > 0.6].count()
         medium_hotspots = hotspots.id[hotspots.owner_id == self.client_id][hotspots.score_v4 < 0.6][hotspots.score_v4 > 0.3].count()
         bad_hotspots = hotspots.id[hotspots.owner_id == self.client_id][hotspots.score_v4 < 0.3].count()
--- tasks/third_task.py
+++ tasks/third_task.py
@@ -3,16 +3,14 @@ from dateutil.relativedelta import relativedelta
 from collections import namedtuple
 
 from data.data import CsvLoader
+from .first_task import FirstTask
 
 DATE_WITHOUT_LAST_YEAR = datetime.now() - relativedelta(years=1)
 DATE_WITHOUT_LAST_MONTH = datetime.now() - relativedelta(months=1)
 DATE_WITHOUT_LAST_WEEK = datetime.now() - timedelta(weeks=1)
 
 
-class ThirdTask():
-
-    def __init__(self, client_id) -> None:
-        self.client_id = client_id
+class ThirdTask(FirstTask):
 
     def replace_string_to_date(cls, date) -> datetime:
         replaced_date = date.replace("T", " ").split(".")[0]
@@ -22,7 +20,7 @@ class ThirdTask():
     def get_hotposts_last_month_week(self):
         last_month_hotposts_count = 0
         last_week_hotposts_count = 0
-        hotspots = CsvLoader.load("hotspots")
+        hotspots = CsvLoader.load(self.hotspots_file)
         for spot in hotspots.itertuples():
             if spot.created_at != " ":
                 hotspots.loc[spot.Index, "created_at"] = self.replace_string_to_date(spot.created_at)
@@ -35,6 +33,6 @@ class ThirdTask():
         result = Hotspot(last_month_hotposts_count, last_week_hotposts_count)
         return result
 
-    
+
 if __name__=="__main__":
     pass
